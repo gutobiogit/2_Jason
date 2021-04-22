d5f18621e38f2d00c9390d7054a9ebfd5396bbe2
Alvaro Locatelli Souza <56000747+gutobiogit@users.noreply.github.com>
2021-04-22 23:08:26 +0200

    Update gtfs_data.py

--- gtfs_data.py
+++ gtfs_data.py
@@ -1,35 +1,16 @@
+from urllib.request import Request, urlopen
+
 from google.transit import gtfs_realtime_pb2
-from urllib2.request import request,urlopen
+
 
 feed = gtfs_realtime_pb2.FeedMessage()
-response = urlopen('https://gtfsr.transportforireland.ie/v1/?format=json?key=AIzaSyCu4ueJS6dPolvynHjJ9QM_iyALJL10Mzc')
+req = Request('https://gtfsr.transportforireland.ie/v1')
+req.add_header('x-api-key', '05a8cca127b04076832bd54d93d46ff2')
+response = urlopen(req)
 feed.ParseFromString(response.read())
+#print (feed.entity[0])
 for entity in feed.entity:
-  if entity.HasField('trip_update'):
-    print (entity.trip_update)
-
-
-
-
-import http.client, urllib.request, urllib.parse, urllib.error, base64
-
-headers = {
-# Request headers
-'Cache-Control': 'no-cache',
-'x-api-key': '05a8cca127b04076832bd54d93d46ff2',
-}
-
-params = urllib.parse.urlencode({
-})
-
-try:
-conn.request("GET", "?%s" % params, headers)
-    response = conn.getresponse()
-    data = response.read()
-    print(data)
-    conn.close()
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
-
-
-
+    if entity.HasField('trip_update'):
+        x=feed.entity
+        print(x)  
+        #print(feed.entity.values)
